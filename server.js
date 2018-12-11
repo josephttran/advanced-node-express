fd8a6f77d5c6a252e41dedb96286e46ff5c612a6
josephttran <42635012+josephttran@users.noreply.github.com>
2018-12-11 04:12:32 -0800

    use passport and session, implement serialization

--- server.js
+++ server.js
@@ -3,6 +3,10 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const path        = require('path');
+const passport    = require('passport');
+const session     = require('express-session');
+const mongo       = require('mongodb').MongoClient;
+const ObjectID    = require('mongodb').ObjectID;
 require('dotenv').config();
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 
@@ -13,6 +17,14 @@ fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+// setup session and passport
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 // setup template engine
 app.set('views', path.join(__dirname, './views/pug'));
 app.set('view engine', 'pug');
@@ -21,7 +33,29 @@ app.get('/', (req, res) => {
   res.render('index', {title: 'Hello', message: 'Please login'});
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  let port = process.env.PORT ? process.env.PORT : 3000;
-  console.log("Listening on port " + port);
+mongo.connect(process.env.DATABASE, { useNewUrlParser: true }, (err, db) => {
+  if (err) {
+    console.log(`Database error: ${err}`);
+  } else {
+    console.log('Successful database connection');
+
+    passport.serializeUser((user, done) => {
+      done(null, user._id);
+    })
+
+    passport.deserializeUser((id, done) => {
+      db.collection('users').findOne(
+        {_id: new ObjectID(id)}, 
+        (err, doc) => {
+          done(null, doc)
+        }
+      );
+    })
+
+    app.listen(process.env.PORT || 3000, () => {
+      let port = process.env.PORT ? process.env.PORT : 3000;
+      console.log("Listening on port " + port);
+    });
+  }
 });
+
