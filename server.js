3c262f160fa999d48f47981828f3d5249427f704
josephttran <42635012+josephttran@users.noreply.github.com>
2018-12-11 03:26:57 -0800

    setup and use template engine

--- server.js
+++ server.js
@@ -2,19 +2,23 @@
 
 const express     = require('express');
 const bodyParser  = require('body-parser');
+const path        = require('path');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
+
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+// setup template engine
+app.set('views', path.join(__dirname, './views/pug'));
+app.set('view engine', 'pug');
 
-app.route('/')
-  .get((req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.get('/', (req, res) => {
+  res.render('index', {title: 'Hello', message: 'Please login'});
+});
 
 app.listen(process.env.PORT || 3000, () => {
   let port = process.env.PORT ? process.env.PORT : 3000;
